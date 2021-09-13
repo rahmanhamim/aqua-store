4e5f5981b0185a894b6cb209565e7f2436b2eee0
rahmanhamim <rahmanhamiminfo@gmail.com>
2021-09-13 16:49:34 +0600

    design retouch

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const loadProducts = () => {
- const url = `https://fakestoreapi.com/products`;
+ const url = `https://raw.githubusercontent.com/ProgrammingHero1/ranga-store-api/main/ranga-api.json`;
  fetch(url)
   .then((response) => response.json())
   .then((data) => showProducts(data));
@@ -13,19 +13,38 @@ const showProducts = (products) => {
   const image = product.image;
   const div = document.createElement("div");
   div.classList.add("product");
-  div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
+  div.innerHTML = `
+      <div class="single-product">
+        <div>
+          <img class="product-image" src=${image}></img>
+        </div>
+        <h3>${product.title}</h3>
+        <h5>Category: ${product.category}</h5>
+        <p><small>Total reviews: ${product.rating.count}</small></p>
+        <p><small>  Average rating: ${product.rating.rate} <i class="fas fa-star" style="color: gold;"></i> </small></p>
+        <h2>Price: $ ${product.price}</h2>
+        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="basic-btn">Add to cart</button>
+        <!-- Button trigger modal -->
+        <button onclick="loadModal('${product.title}', '${product.image}', '${product.description}')" type="button" class="second-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          Details
+        </button>
       </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
   document.getElementById("all-products").appendChild(div);
  }
 };
+// Display Modal function
+const loadModal = (title, image, description) => {
+ const moadlTitle = document.getElementById("modal-title");
+ const modalImage = document.getElementById("modal-image");
+ const modalDescription = document.getElementById("modal-description");
+
+ moadlTitle.innerHTML = title;
+ modalImage.src = image;
+ modalDescription.innerText = description;
+};
+
+// Adding Info To My Cart
 let count = 0;
 const addToCart = (id, price) => {
  count = count + 1;
@@ -37,6 +56,7 @@ const addToCart = (id, price) => {
  updateTotal();
 };
 
+// Getting Input Value
 const getInputValue = (id) => {
  const element = document.getElementById(id).innerText;
  const converted = parseFloat(element);
@@ -79,7 +99,5 @@ const updateTotal = () => {
   getInputValue("price") +
   getInputValue("delivery-charge") +
   getInputValue("total-tax");
-
- console.log(grandTotal);
  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
