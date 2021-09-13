b793beb9339e59ed26ca15f45bc87b2bfa95b640
rahmanhamim <rahmanhamiminfo@gmail.com>
2021-09-13 17:14:31 +0600

    project done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const loadProducts = () => {
- const url = `https://raw.githubusercontent.com/ProgrammingHero1/ranga-store-api/main/ranga-api.json`;
+ const url = `https://fakestoreapi.com/products`;
  fetch(url)
   .then((response) => response.json())
   .then((data) => showProducts(data));
